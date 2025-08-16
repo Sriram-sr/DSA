d152ff2201744d31c473ce2567ec6ca0c2c732cf
Sriram-sr <srirampanneer90@gmail.com>
2025-08-17 00:24:19 +0530

    DSA - more patterns

--- Patterns.java
+++ Patterns.java
@@ -1,18 +1,74 @@
 class Solution {
     /*
      * """
-        Write a program to print the below pattern
+     * Write a program to print the below pattern
+     *****
+     ****
+     ***
+     **
+     *
+     * """
+     */
+    public void pattern4(int n) {
+        for (int row = 1; row <= n; row++) {
+            for (int col = 1; col <= n - row + 1; col++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
 
-        ****
-        ****
-        ****
-        ****
+    /*
+     * """
+     * Write a program to print the below pattern
+     *
+     ***
+     *****
+     *******
+     *********
+     * """
+     */
+    public void pattern3(int n) {
+        for (int row = 1; row <= n; row++) {
+            for (int col = 1; col <= row * 2 - 1; col++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
 
-        """
-    */
+    /*
+     * """
+     * Write a program to print the below pattern
+     *
+     **
+     ***
+     ****
+     ***** 
+     * """
+     */
+    public void pattern2(int n) {
+        for (int row = 1; row <= n; row++) {
+            for (int col = 1; col <= row; col++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+
+    /*
+     * """
+     * Write a program to print the below pattern
+     *****
+     *****
+     *****
+     *****
+     *****
+     * """
+     */
     public void pattern1(int n) {
-        for (int row = 0; row < n; row++) {
-            for (int col = 0; col < n; col++) {
+        for (int row = 1; row <= n; row++) {
+            for (int col = 1; col <= n; col++) {
                 System.out.print("*");
             }
             System.out.println();
@@ -24,6 +80,6 @@ public class Patterns {
 
     public static void main(String[] args) {
         Solution solution = new Solution();
-        solution.pattern1(4);
+        solution.pattern4(5);
     }
 }
