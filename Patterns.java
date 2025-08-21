45a6064cc955485974ef81251ffee63a4b5dca82
Sriram-sr <srirampanneer90@gmail.com>
2025-08-21 23:30:46 +0530

    DSA - one pattern from hard

--- Patterns.java
+++ Patterns.java
@@ -2,6 +2,31 @@ class Solution {
     /*
      *
      * Write a program to print the below pattern
+        1        1
+        12      21
+        123    321
+        1234  4321
+        1234554321
+    *
+    */
+    public void pattern14(int n) {
+        for (int i = 1; i <=n; i++) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print(j);
+            }
+            for (int k = 1; k <= (2 * n) - (2 * i); k++) {
+                System.out.print(" ");
+            }
+            for (int l = i; l >= 1; l--) {
+                System.out.print(l);
+            }
+            System.out.println();
+        }
+    }
+
+    /*
+     *
+     * Write a program to print the below pattern
         1 
         0 1
         1 0 1
@@ -300,6 +325,6 @@ public class Patterns {
 
     public static void main(String[] args) {
         Solution solution = new Solution();
-        solution.pattern13(5);
+        solution.pattern14(4);
     }
 }
