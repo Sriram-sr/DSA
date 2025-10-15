33e667b9dbe4e70a295958f269dac3fada2e62f8
Sriram-sr <srirampanneer90@gmail.com>
2025-10-16 00:17:26 +0530

    Programs - Isomorphic string

--- StringPrbs.java
+++ StringPrbs.java
@@ -21,12 +21,26 @@ class StringSolutions {
             System.out.println(c);
         }
     }
+
+    public boolean isomorphicString(String s, String t) {
+        int[] mapS = new int[256], mapT = new int[256];
+
+        for (int idx = 0; idx < s.length(); idx++) {
+            if (mapS[s.charAt(idx)] != mapT[t.charAt(idx)]) return false;
+
+            mapS[s.charAt(idx)] = idx + 1;
+            mapT[t.charAt(idx)] = idx + 1;
+        }
+
+        return true;
+    }
 }
 
 public class StringPrbs {
     public static void main(String[] args) {
         StringSolutions solution = new StringSolutions();
         List<Character> s = new ArrayList<>(Arrays.asList('h', 'e', 'l', 'l', 'o'));
-        solution.reverseString(s);
+        // solution.reverseString(s);
+        System.out.println(solution.isomorphicString("ab", "aa"));
     }
 }
