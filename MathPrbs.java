0e4777ed58bce6a2772930858ba9f1e6ed2a3b06
Sriram-sr <srirampanneer90@gmail.com>
2025-10-19 18:02:05 +0530

    DSA - Math Array and string modules problems

---
+++ MathPrbs.java
@@ -0,0 +1,295 @@
+import java.util.Arrays;
+
+class MathSolutions {
+    // Program to find the sum of digits
+
+    public int sumOfDigits(int num) {
+        int rem, digitsSum = 0;
+
+        while (num >= 1) {
+            rem = num % 10;
+            digitsSum += rem;
+            num /= 10;
+        }
+
+        return digitsSum;
+    }
+
+    // Count number of odd digits in a number
+
+    public int countOddDigit(int n) {
+        int oddCount = 0;
+        int rem;
+
+        while (n > 0) {
+            rem = n % 10;
+            if (rem % 2 != 0) {
+                oddCount += 1;
+            }
+            n /= 10;
+        }
+
+        return oddCount;
+    }
+
+    // Reverse a number
+
+    public int reverseNumber(int num) {
+        long reversed = 0;
+        int remainder = 0;
+        while (num != 0) {
+            remainder = num % 10;
+            reversed = (reversed * 10) + remainder;
+            num /= 10;
+        }
+
+        if (reversed > Integer.MAX_VALUE || reversed < Integer.MIN_VALUE) {
+            return 0;
+        }
+        if (num < 0) {
+            return (int) (-1 * reversed);
+        }
+
+        return (int) reversed;
+    }
+
+    // Palindrome Number
+
+    public boolean isPalindrome(int n) {
+        int original = n;
+        int reversed = 0;
+        int remainder = 0;
+        while (n >= 1) {
+            remainder = n % 10;
+            reversed = (reversed * 10) + remainder;
+            n /= 10;
+        }
+
+        if (original == reversed) {
+            return true;
+        }
+
+        return false;
+    }
+
+    // Return the Largest Digit in a Number
+
+    public int getLargest(int n) {
+        int largest = 0, rem;
+
+        while (n > 0) {
+            rem = n % 10;
+            if (rem > largest) {
+                largest = rem;
+            }
+            n /= 10;
+        }
+
+        return largest;
+    }
+
+    // Factorial of a given number
+
+    public int factorial(int n) {
+        if (n == 0) {
+            return 1;
+        }
+
+        int factorial = 1;
+        for (int ele = 1; ele <= n; ele++) {
+            factorial *= ele;
+        }
+
+        return factorial;
+    }
+
+    // Program to check whether a given number is Armstrong or not
+
+    public boolean isAmstrongNumber(int num) {
+        int length = String.valueOf(num).length();
+        int armstrongValue = 0;
+        int originalValue = num;
+        int remainder;
+        while (num > 0) {
+            remainder = num % 10;
+            armstrongValue += Math.pow(remainder, length);
+            num /= 10;
+        }
+        if (armstrongValue == originalValue) {
+            return true;
+        }
+
+        return false;
+    }
+
+    // Check for Perfect Number
+
+    public boolean isPerfect(int n) {
+        // Brute
+
+        /*
+         * int perfectNumber = 0;
+         * for (int ele = 1; ele < n; ele++) {
+         * if (n % ele == 0) {
+         * perfectNumber += ele;
+         * }
+         * }
+         * 
+         * if (n == perfectNumber) {
+         * return true;
+         * }
+         * 
+         * return false;
+         * 
+         */
+
+        // Optimal
+
+        int perfectNum = n != 1 ? 1 : 0;
+
+        for (int ele = 2; ele * ele <= n; ele++) {
+            if (n % ele == 0 && n / ele != ele) {
+                perfectNum += ele;
+                perfectNum += n / ele;
+            }
+        }
+
+        return perfectNum == n;
+    }
+
+    // Check for Prime Number
+
+    public static boolean isPrime(int n) {
+        // Brute
+
+        // if (n == 1) {
+        // return false;
+        // }
+        // for (int i = 2; i < n; i++) {
+        // if (n % i == 0) {
+        // return false;
+        // }
+        // }
+        // return true;
+
+        // Optimal
+
+        if (n == 1) {
+            return false;
+        }
+
+        for (int i = 2; i * i <= n; i++) {
+            if (n % i == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    // Count of Prime Numbers till N
+
+    public int countPrimeNumbers(int n) {
+        int count = 0;
+
+        for (int i = 2; i <= n; i++) {
+            if (MathSolutions.isPrime(i)) {
+                count += 1;
+            }
+        }
+
+        return count;
+    }
+
+    // GCD of Two Numbers
+
+    public static int gcdOfTwoNumbers(int n1, int n2) {
+        // Brute
+
+        // int gcd = 1;
+        // int min = n1 < n2 ? n1 : n2;
+
+        // for (int ele = 1; ele <= min; ele++) {
+        // if (n1 % ele == 0 && n2 % ele == 0) {
+        // gcd = ele;
+        // }
+        // }
+
+        // return gcd;
+
+        // Optimal
+
+        while (n1 != 0 && n2 != 0) {
+            if (n1 > n2) {
+                n1 = n1 % n2;
+            } else {
+                n2 = n2 % n1;
+            }
+        }
+
+        return n1 != 0 ? n1 : n2;
+    }
+
+    // LCM of two numbers
+
+    public int lcmOfNumbers(int n1, int n2) {
+        // Brute
+
+        // int max = n1 > n2 ? n1 : n2;
+        // int count = 1, lcm;
+
+        // do {
+        // lcm = count * max;
+        // if (lcm % n1 == 0 && lcm % n2 == 0) {
+        // break;
+        // }
+        // count++;
+        // } while (count != 0);
+
+        // return lcm;
+
+        // Optimal
+
+        return (n1 * n2) / MathSolutions.gcdOfTwoNumbers(n1, n2);
+    }
+
+    // Divisors of a Number
+
+    public int[] getDivisors(int n) {
+        // int[] list = new int[n];
+
+        // int count = 0;
+        // for (int i = 1; i <= n; i++) {
+        // if (n % i == 0) {
+        // list[count] = i;
+        // count++;
+        // }
+        // }
+
+        // return Arrays.copyOf(list, count);
+
+        // Optimal
+
+        int count = 0;
+        int temp[] = new int[(int) Math.sqrt(n) * 2];
+
+        for (int num = 1; num * num <= n; num++) {
+            if (n % num == 0) {
+                temp[count++] = num;
+                if (num * num != n) {
+                    temp[count++] = n / num;
+                }
+            }
+        }
+
+        Arrays.sort(temp, 0, count);
+
+        return Arrays.copyOf(temp, count);
+    }
+}
+
+public class MathPrbs {
+    public static void main(String[] args) {
+        MathSolutions mathSolutions = new MathSolutions();
+        System.out.println(mathSolutions.sumOfDigits(1234));
+    }
+}
--- StringPrbs.java
+++ StringPrbs.java
@@ -2,7 +2,90 @@ import java.util.Arrays;
 import java.util.ArrayList;
 import java.util.List;
 
+class Pair {
+    int freq;
+    char ele;
+
+    Pair(int freq, char ele) {
+        this.freq = freq;
+        this.ele = ele;
+    }
+}
+
 class StringSolutions {
+    // Sum of Array Elements
+
+    public  int sum(int arr[], int n) {
+      int elementsSum = 0;
+
+      for (int idx = 0; idx < n; idx++) {
+        elementsSum += arr[idx];
+      }
+
+      return elementsSum;
+    }
+
+    // Count Odd Numbers in an Array
+
+    public int countOdd(int[] arr, int n) {
+       int count = 0;
+
+       for (int idx = 0; idx < n; idx++) {
+        if (arr[idx] % 2 == 1) {
+            count += 1;
+        }
+       }
+
+       return count;
+    }
+
+    // Check if Array is Sorted
+
+    public boolean arraySortedOrNot(int[] arr, int n) {
+        for (int idx = 0; idx <= n - 2; idx++) {
+            if (!(arr[idx] <= arr[idx + 1])) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    // Reverse an array
+
+    public int[] reverseArray(int[] arr, int n) {
+        // Brute
+
+        // int[] tempArray = new int[n];
+
+        // for (int idx = n - 1; idx >= 0; idx--) {
+        // tempArray[n - 1 - idx] = arr[idx];
+        // }
+
+        // for (int idx = 0; idx < n; idx++) {
+        // arr[idx] = tempArray[idx];
+        // }
+
+        // return arr;
+
+        // Optimal
+
+        int left = 0;
+        int right = n - 1;
+        int temp;
+
+        while (right > left) {
+            temp = arr[right];
+            arr[right] = arr[left];
+            arr[left] = temp;
+            left++;
+            right--;
+        }
+
+        return arr;
+    }
+
+    // Reverse String
+
     public void reverseString(List<Character> s) {
         int left = 0;
         int right = s.size() - 1;
@@ -22,11 +105,68 @@ class StringSolutions {
         }
     }
 
+    // Palindrome Check string
+
+    public boolean palindromeCheck(String s) {
+        // int lastIdx = s.length() - 1;
+        // StringBuilder reverse = new StringBuilder();
+
+        // while (lastIdx >= 0) {
+        // reverse.append(s.charAt(lastIdx));
+        // lastIdx--;
+        // }
+
+        // return s.equals(reverse.toString());
+
+        // Approach 2
+
+        int left = 0;
+        int right = s.length() - 1;
+
+        while (left < right) {
+            if (s.charAt(left) != s.charAt(right)) {
+                return false;
+            }
+            left++;
+            right--;
+        }
+
+        return true;
+    }
+
+    // Largest Odd Number in a String
+
+    public String largestOddNumberString(String s) {
+        int n = s.length();
+        int endIdx = -1;
+        for (int idx = n - 1; idx >= 0; idx--) {
+            if ((s.charAt(idx) - '0') % 2 == 1) {
+                endIdx = idx;
+                break;
+            }
+        }
+        if (endIdx == -1) {
+            return "";
+        }
+
+        int startIdx = 0;
+        for (startIdx = 0; startIdx < n; startIdx++) {
+            if (s.charAt(startIdx) != '0') {
+                break;
+            }
+        }
+
+        return s.substring(startIdx, endIdx + 1);
+    }
+
+    // Isomorphic Strings
+
     public boolean isomorphicString(String s, String t) {
         int[] mapS = new int[256], mapT = new int[256];
 
         for (int idx = 0; idx < s.length(); idx++) {
-            if (mapS[s.charAt(idx)] != mapT[t.charAt(idx)]) return false;
+            if (mapS[s.charAt(idx)] != mapT[t.charAt(idx)])
+                return false;
 
             mapS[s.charAt(idx)] = idx + 1;
             mapT[t.charAt(idx)] = idx + 1;
@@ -34,13 +174,94 @@ class StringSolutions {
 
         return true;
     }
+
+    // Rotate String
+
+    public boolean rortateString(String s, String goal) {
+        // Brute
+
+        if (s.length() != goal.length()) {
+            return false;
+        }
+
+        if (s.length() == 1) {
+            return s.equals(goal);
+        }
+
+        int shifts = s.length();
+        while (shifts > 0) {
+            s = s.substring(1) + s.charAt(0);
+            if (s.equals(goal)) {
+                return true;
+            }
+            shifts--;
+        }
+
+        return false;
+
+        // Optimimal
+
+        // return s.length() == goal.length() && (s + s).contains(goal);
+    }
+
+    // Valid Anagram
+
+    public boolean isAnagram(String s, String t) {
+        if (s.length() != t.length()) {
+            return false;
+        }
+
+        int[] freqS = new int[26], freqT = new int[26];
+        for (int idx = 0; idx < s.length(); idx++) {
+            freqS[s.charAt(idx) - 'a']++;
+            freqT[t.charAt(idx) - 'a']++;
+        }
+
+        for (int charIdx = 0; charIdx < freqS.length; charIdx++) {
+            if (freqS[charIdx] != freqT[charIdx]) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+    // Sort Characters By Frequency
+
+    public List<Character> sortByFrequency(String s) {
+        Pair[] hash = new Pair[26];
+
+        for (int idx = 0; idx < hash.length; idx++) {
+            hash[idx] = new Pair(0, (char) (idx + 'a'));
+        }
+
+        for (int idx = 0; idx < s.length(); idx++) {
+            hash[s.charAt(idx) - 'a'].freq++;
+        }
+
+        Arrays.sort(hash, (p1, p2) -> {
+            if (p1.freq != p2.freq)
+                return p2.freq - p1.freq;
+            return p1.ele - p2.ele;
+        });
+
+        List<Character> result = new ArrayList<>();
+        for (int idx = 0; idx < hash.length; idx++) {
+            if (hash[idx].freq == 0) {
+                break;
+            }
+            result.add(hash[idx].ele);
+        }
+
+        return result;
+    }
 }
 
 public class StringPrbs {
     public static void main(String[] args) {
         StringSolutions solution = new StringSolutions();
-        List<Character> s = new ArrayList<>(Arrays.asList('h', 'e', 'l', 'l', 'o'));
-        // solution.reverseString(s);
-        System.out.println(solution.isomorphicString("ab", "aa"));
+        // solution.reverseString(new ArrayList<>(Arrays.asList('h', 'e', 'l', 'l',
+        // 'o')));
+        // System.out.println(solution.isomorphicString("ab", "aa"));
+        System.out.println(solution.sortByFrequency("bbccddaaa"));
     }
 }
