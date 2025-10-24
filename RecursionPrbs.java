c72cc3e11a87ecf19a7b57eae1cd14a4ced03fd9
Sriram-sr <srirampanneer90@gmail.com>
2025-10-24 13:18:57 +0530

    DSA - more recursion problems

--- RecursionPrbs.java
+++ RecursionPrbs.java
@@ -1,3 +1,6 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+
 class RecursionSolutions {
     // Sum of First N Numbers
     public int sumOfNaturalNumbers(int N) {
@@ -14,11 +17,94 @@ class RecursionSolutions {
 
         return (long) n * factorial(n - 1);
     }
+
+    public int sum(int[] nums, int idx) {
+        if (idx < 0) {
+            return 0;
+        }
+
+        return nums[idx] + sum(nums, idx - 1);
+    }
+
+    // Sum of Array Elements
+    public int arraySum(int[] nums) {
+        return sum(nums, nums.length - 1);
+    }
+
+    public void swapCharacters(ArrayList<Character> list, int left, int right) {
+        if (left >= right) {
+            return;
+        }
+
+        char temp = list.get(right);
+        list.set(right, list.get(left));
+        list.set(left, temp);
+        swapCharacters(list, left + 1, right - 1);
+    }
+
+    // Reverse a String
+    public ArrayList<Character> reverseString(ArrayList<Character> s) {
+        swapCharacters(s, 0, s.size() - 1);
+        return s;
+    }
+
+    public boolean reverse(String s, int left, int right) {
+        if (left >= right)
+            return true;
+
+        if (s.charAt(left) != s.charAt(right)) {
+            return false;
+        }
+
+        return reverse(s, left + 1, right - 1);
+    }
+
+    // Check if String is Palindrome or Not
+    public boolean palindromeCheck(String s) {
+        return reverse(s, 0, s.length() - 1);
+    }
+
+    public boolean isDivisible(int n, int div) {
+        if (div * div > n)
+            return true;
+
+        if (n % div == 0)
+            return false;
+
+        return isDivisible(n, div + 1);
+    }
+
+    // Check if a Number is Prime or Not
+    public boolean checkPrime(int num) {
+        if (num == 1)
+            return false;
+        if (num == 2)
+            return true;
+
+        return isDivisible(num, 2);
+    }
+
+    public void swapIntegers(int[] nums, int left, int right) {
+        if (left > right)
+            return;
+
+        int temp = nums[right];
+        nums[right] = nums[left];
+        nums[left] = temp;
+
+        swapIntegers(nums, left + 1, right - 1);
+    }
+
+    // Reverse an array
+    public int[] reverseArray(int[] nums) {
+        swapIntegers(nums, 0, nums.length - 1);
+        return nums;
+    }
 }
 
 public class RecursionPrbs {
     public static void main(String[] args) {
         RecursionSolutions solution = new RecursionSolutions();
-        System.out.println(solution.factorial(3));
+        System.out.println(Arrays.toString(solution.reverseArray(new int[] { 1, 2, 3, 4, 5 })));
     }
 }
