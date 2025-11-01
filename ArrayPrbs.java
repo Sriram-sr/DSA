a5f4fec1b8c1ed1a20946b7c8116875b51ccce11
Sriram-sr <srirampanneer90@gmail.com>
2025-11-02 00:54:05 +0530

    DSA - some Array mid level problems

--- ArrayPrbs.java
+++ ArrayPrbs.java
@@ -2,6 +2,9 @@ import java.util.Arrays;
 import java.util.Set;
 import java.util.TreeSet;
 import java.util.ArrayList;
+import java.util.Map;
+import java.util.HashMap;
+import java.util.List;
 
 class ArraySolutions {
     // Linear Search
@@ -332,13 +335,244 @@ class ArraySolutions {
 
         return result;
     }
+
+    // Majority Element-I
+    public int majorityElement(int[] nums) {
+        // Brute force: O(n^2) time, O(1) space
+
+        // int half = nums.length / 2, count;
+
+        // for (int idx = 0; idx < nums.length; idx++) {
+        // count = 0;
+        // for (int subIdx = 0; subIdx < nums.length; subIdx++) {
+        // if (nums[idx] == nums[subIdx])
+        // count++;
+        // }
+
+        // if (count > half)
+        // return nums[idx];
+        // }
+
+        // return -1;
+
+        // Better: O(n) time, O(n) space
+
+        // int half = nums.length / 2;
+        // HashMap<Integer, Integer> map = new HashMap<>();
+
+        // for (int ele : nums) {
+        // map.put(ele, map.getOrDefault(ele, 0) + 1);
+        // }
+
+        // for (Map.Entry<Integer, Integer> entry : map.entrySet()) {
+        // if (entry.getValue() > half)
+        // return entry.getKey();
+        // }
+
+        // return -1;
+
+        // Optimal: O(n) time, O(1) space
+
+        int ele = 0, count = 0;
+
+        for (int idx = 0; idx < nums.length; idx++) {
+            if (count == 0)
+                ele = nums[idx];
+            if (nums[idx] == ele) {
+                count++;
+            } else {
+                count--;
+            }
+        }
+
+        return ele;
+    }
+
+    // Leaders in an Array
+    public List<Integer> leaders(int[] nums) {
+        // Brute: O(n^2) time, O(n) space
+        // List<Integer> leaders = new ArrayList<>();
+        // boolean isLeader;
+
+        // for (int idx = 0; idx < nums.length; idx++) {
+        // isLeader = true;
+        // for (int subIdx = idx + 1; subIdx < nums.length; subIdx++) {
+        // if (nums[subIdx] >= nums[idx]) {
+        // isLeader = false;
+        // break;
+        // }
+        // }
+        // if (isLeader) {
+        // leaders.add(nums[idx]);
+        // }
+        // }
+
+        // return leaders;
+
+        // Optimal: O(n) time, O(n) space
+
+        List<Integer> leaders = new ArrayList<>();
+        int max = Integer.MIN_VALUE;
+
+        for (int idx = nums.length - 1; idx >= 0; idx--) {
+            if (nums[idx] > max) {
+                max = nums[idx];
+                leaders.add(nums[idx]);
+            }
+        }
+
+        // Reverse for correct order
+        int left = 0, right = leaders.size() - 1, temp;
+        while (left < right) {
+            temp = leaders.get(right);
+            leaders.set(right, leaders.get(left));
+            leaders.set(left, temp);
+            left++;
+            right--;
+        }
+
+        return leaders;
+    }
+
+    // Rearrange array elements by sign
+    public int[] rearrangeArray(int[] nums) {
+        // Brute: O(2n) time, O(n) space
+
+        // List<Integer> positives = new ArrayList<>();
+        // List<Integer> negatives = new ArrayList<>();
+
+        // for (int idx = 0; idx < nums.length; idx++) {
+        // if (nums[idx] < 0)
+        // negatives.add(nums[idx]);
+        // else
+        // positives.add(nums[idx]);
+        // }
+
+        // for (int idx = 0; idx < nums.length / 2; idx++) {
+        // nums[2 * idx] = positives.get(idx);
+        // nums[2 * idx + 1] = negatives.get(idx);
+        // }
+
+        // return nums;
+
+        // Optimal: O(n) time, O(n) space
+
+        int[] result = new int[nums.length];
+        int positivePtr = 0, negativePtr = 1;
+
+        for (int idx = 0; idx < nums.length; idx++) {
+            if (nums[idx] < 0) {
+                result[negativePtr] = nums[idx];
+                negativePtr += 2;
+            } else {
+                result[positivePtr] = nums[idx];
+                positivePtr += 2;
+            }
+        }
+
+        return result;
+    }
+
+    // Print the matrix in spiral manner
+    public List<Integer> spiralOrder(int[][] matrix) {
+        // Optimal: O(row * col) time and O(row * col) space
+
+        int rows = matrix.length;
+        int columns = matrix[0].length;
+        int left = 0, top = 0;
+        int right = columns - 1, bottom = rows - 1;
+        List<Integer> result = new ArrayList<>();
+
+        while (left <= right && top <= bottom) {
+            for (int ptr = left; ptr <= right; ptr++) {
+                result.add(matrix[top][ptr]);
+            }
+            top++;
+            for (int ptr = top; ptr <= bottom; ptr++) {
+                result.add(matrix[ptr][right]);
+            }
+            right--;
+            if (top <= bottom) {
+                for (int ptr = right; ptr >= left; ptr--) {
+                    result.add(matrix[bottom][ptr]);
+                }
+                bottom--;
+            }
+            if (left <= right) {
+                for (int ptr = bottom; ptr >= top; ptr--) {
+                    result.add(matrix[ptr][left]);
+                }
+                left++;
+            }
+        }
+
+        return result;
+    }
+
+    public int nCr(int n, int r) {
+        if (r == 1)
+            return n;
+
+        int answer = 1;
+
+        for (int idx = 0; idx < r; idx++) {
+            answer *= n - idx;
+            answer /= idx + 1;
+        }
+
+        return answer;
+    }
+
+    // Pascal's Triangle I
+    public int pascalTriangleI(int r, int c) {
+        // Optimal O(r) time and O(1) space
+        return nCr(r - 1, c - 1);
+    }
+
+    // Pascal's Triangle II
+    public int[] pascalTriangleII(int r) {
+        // Optimal O(r) time and O(r) space
+
+        int temp = 1;
+        int[] result = new int[r];
+        result[0] = 1;
+
+        for (int idx = 1; idx < r; idx++) {
+            temp *= r - idx;
+            temp /= idx;
+            result[idx] = temp;
+        }
+
+        return result;
+    }
+
+    public List<Integer> generatePascalRow(int r) {
+        int temp = 1;
+        List<Integer> result = new ArrayList<>();
+        result.add(1);
+
+        for (int idx = 1; idx < r; idx++) {
+            temp *= r - idx;
+            temp /= idx;
+            result.add(temp);
+        }
+
+        return result;
+    }
+
+    public List<List<Integer>> pascalTriangleIII(int n) {
+        List<List<Integer>> fullPascalRows = new ArrayList<>();
+        for (int row = 1; row <= n; row++) {
+            fullPascalRows.add(generatePascalRow(row));
+        }
+
+        return fullPascalRows;
+    }
 }
 
 public class ArrayPrbs {
     public static void main(String[] args) {
         ArraySolutions solution = new ArraySolutions();
-        System.out.println(
-                Arrays.toString(
-                        solution.intersectionArray(new int[] { 1, 2, 2, 3, 3, 3 }, new int[] { 2, 3, 3, 4, 5, 7 })));
+        System.out.println(solution.pascalTriangleIII(6));
     }
 }
