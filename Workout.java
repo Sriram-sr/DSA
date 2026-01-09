16daf8455ad302687ec681f093cb39814ae071ec
Sriram-sr <srirampanneer90@gmail.com>
2026-01-09 23:45:52 +0530

    DSA - removing str code

--- Workout.java
+++ Workout.java
@@ -406,23 +406,6 @@ class WorkoutSolutions {
 
         return high;
     }
-
-    public int minimumRateToEatBananas(int[] nums, int h) {
-        // Initialize binary search bounds
-        int low = 1, high = findMax(nums);
-
-        // Apply binary search
-        while (low <= high) {
-            int mid = (low + high) / 2;
-            long totalH = calculateTotalHours(nums, mid);
-            if (totalH <= h) {
-                high = mid - 1;
-            } else {
-                low = mid + 1;
-            }
-        }
-        return low;
-    }
 }
 
 public class Workout {
